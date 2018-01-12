37b52b396c34bdfe48971b0a5db7c489fefddb24
mconiaris <mconiaris@gmail.com>
2018-01-12 11:01:40 -0500

    Ran Yeoman angular scaffold.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name americanPhotosApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the americanPhotosApp
+ */
+angular.module('americanPhotosApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
